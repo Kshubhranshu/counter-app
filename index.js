4d5b990b132b0e1994ada8a32bae8badbe321a39
kumaar <kumaar@DESKTOP-UL0FUTT.localdomain>
2023-01-09 20:13:30 +0530

    first commit

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function reset() {
     data = 0;
     document.getElementById("counting").innerText = data;
 }
+
+function decrement() {
+    data = data - 1;
+    data = data < 0 ? 0 : data;
+    document.getElementById("counting").innerText = data;
+}
